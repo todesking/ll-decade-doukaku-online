e32199463961c81bd5213560fca5073ad8d24cf6
todesking <discommunicative@gmail.com>
2012-08-04 12:24:40 +0900

    ipv4

--- llnocrypt.rb
+++ llnocrypt.rb
@@ -1,6 +1,13 @@
 class LLNOCrypt
   class Distincter
     def ipv4? str
+      str.split('.').each do|num_str|
+        return false unless num_str =~ /^\d+$/
+        num = num_str.to_i
+        return false unless 0 <= num && num <= 255
+        return false if num_str =~ /^0\d+/
+      end
+      return true
     end
   end
 end
