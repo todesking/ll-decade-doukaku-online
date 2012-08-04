29fbddb977719aa310de7c7ab9414a5d951034d6
todesking <discommunicative@gmail.com>
2012-08-04 12:48:00 +0900

    mac

--- llnocrypt.rb
+++ llnocrypt.rb
@@ -4,9 +4,9 @@ class LLNOCrypt
       return false if str.empty?
       str.split('.').each do|num_str|
         return false unless num_str =~ /^\d+$/
+        return false if num_str =~ /^0\d+/
         num = num_str.to_i
         return false unless 0 <= num && num <= 255
-        return false if num_str =~ /^0\d+/
       end
       return true
     end
@@ -14,9 +14,21 @@ class LLNOCrypt
       return false if str.empty?
       str.split(':').each do|num_str|
         return false unless num_str =~ /^[0-9A-Fa-f]+$/
+        return false if num_str =~ /^0.+/
         num = num_str.to_i(16)
         return false unless 0x0000 <= num && num <= 0xFFFF
+      end
+      return true
+    end
+    def mac? str
+      return false if str.empty?
+      return false unless str =~ /([-:])/
+      separator = $1
+      str.split(separator).each do|num_str|
+        return false unless num_str =~/^[0-9A-Fa-f]+$/
         return false if num_str =~ /^0.+/
+        num = num_str.to_i(16)
+        return false unless 0x0000 <= num && num <= 0xFFFF
       end
       return true
     end
--- llnocrypt_spec.rb
+++ llnocrypt_spec.rb
@@ -17,6 +17,7 @@ describe LLNOCrypt do
       it '無駄なゼロがついてたらIPv4ではない' do
         should_not be_ipv4 '00.0.0.0'
       end
+      it '数字の並びは4つ'
     end
     describe '#ipv6?' do
       it '文字列がIPv6かどうか判別できる' do
@@ -34,9 +35,25 @@ describe LLNOCrypt do
       it '小文字も受理する' do
         should be_ipv6 'ABCd:1234:5678:ffff'
       end
+      it '数字の並びは8つ'
     end
     describe '#mac?' do
-      it '文字列がMACアドレスかどうか判別できる'
+      it '文字列がMACアドレスかどうか判別できる' do
+        should be_mac '0:11:22:333:4444:FFFF'
+        should be_mac '1A-B2-c3-d4-e5-f6'
+        should_not be_mac 'XX:11:22:33:44:55'
+        should_not be_mac ''
+      end
+      it '空文字列は受理しない' do
+        should_not be_mac ''
+      end
+      it '無駄なゼロがついていたらMACではない' do
+        should_not be_mac 'aa-bb-cc-dd-ee-00'
+      end
+      it '区切り文字は統一されてる' do
+        should_not be_mac '1A-B2:c3-d4-e5-f6'
+      end
+      it '数字の並びは6つ'
     end
   end
   describe '#decode_bits_to_char' do
